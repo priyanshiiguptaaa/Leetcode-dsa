16883343a1fca768e07c610bb3614fdab834a5eb
Priyanshi Gupta <152763463+priyanshiiguptaaa@users.noreply.github.com>
2025-04-04 16:52:18 +0530

    Create 96. Unique Binary Search TreesTABULATION.java

---
+++ 96. Unique Binary Search TreesTABULATION.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numTrees(int n) {
+        if(n == 0 || n == 1) return 1;
+        int dp[] = new int[n + 1];
+        dp[0] = 1; 
+        dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                int left = dp[j - 1];
+                int right = dp[i - j];
+                dp[i] += left*right;
+            }
+        }
+        return dp[n];
+    }
+}
