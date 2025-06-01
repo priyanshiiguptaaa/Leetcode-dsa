559f4fc32db0d8ea93fd57a08cbd2152e88cf197
Priyanshi Gupta <152763463+priyanshiiguptaaa@users.noreply.github.com>
2025-06-01 21:43:59 +0530

    Create 44. Wildcard Matching.java

---
+++ 44. Wildcard Matching.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length(), m = p.length();
+        
+        boolean[] prev = new boolean[m + 1];
+        boolean[] curr = new boolean[m + 1];
+
+        prev[0] = true;
+
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*') {
+                prev[j] = prev[j - 1];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            curr[0] = false;  
+            
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    curr[j] = prev[j - 1]; 
+                } else if (p.charAt(j - 1) == '*') {
+                    curr[j] = curr[j - 1] || prev[j]; 
+                } else {
+                    curr[j] = false;
+                }
+            }
+            prev = curr.clone();
+        }
+
+        return prev[m];
+    }
+}
